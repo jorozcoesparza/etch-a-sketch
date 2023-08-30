fd33c287ad5bbbb51b51920cf643b4da20a5c04d
jorozcoesparza <jorozco842@gmail.com>
2023-08-29 19:29:07 -0500

    Allows user to reize canvas

--- script.js
+++ script.js
@@ -15,3 +15,36 @@ cell.forEach(cell => {
     });
 
 });
+
+function resizing() {
+    let resizeVar = prompt("Enter resize value between 1-100")
+    if (resizeVar < 1 || resizeVar > 100) {
+        alert("Range not valid");
+    } else {
+        let removeCells = document.getElementsByClassName('cell');
+        while(removeCells[0]) {
+            removeCells[0].parentNode.removeChild(removeCells[0]);
+        }
+
+        for (let i = 0; i < resizeVar * resizeVar; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            container.appendChild(cell);
+        }
+
+        const cell = document.querySelectorAll('.cell');
+
+        cell.forEach(cell => {
+            cell.addEventListener('mouseover', () => {
+                cell.classList.add('hoverState');
+            });
+        
+        });
+
+    };
+
+}
+
+const button = document.querySelector("button");
+button.addEventListener('click', resizing);
+
